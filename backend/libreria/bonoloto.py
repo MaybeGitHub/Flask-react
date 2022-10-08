47a3b6913d409f76f80beae34fa889d4d2006d96
Jesusito <jesus@pepito.com>
2022-10-08 18:07:48 +0200

    se ha modificado el componente de react Bonoloto.js para que se use como una funcion en vez de una clase

--- backend/libreria/bonoloto.py
+++ backend/libreria/bonoloto.py
@@ -2,7 +2,7 @@ from libreria.utils.numeros_unicos import generar_numeros_unicos
 
 
 def num_bonoloto():
-    
+
     # result = []
     # for _ in range(6):
     #     new_number = generar_numeros_unicos(result, 1, 49, 1)
@@ -14,7 +14,7 @@ def num_bonoloto():
 
     result = {
         "numeros": [],
-        "complementario": [],
+        "complementarios": [],
     }
 
     for _ in range(6):
@@ -23,8 +23,8 @@ def num_bonoloto():
 
     result["numeros"] = sorted(result["numeros"])
 
-    new_number = generar_numeros_unicos(result["complementario"], 0, 49, 1)
-    result["complementario"].append(new_number)
+    new_number = generar_numeros_unicos(result["complementarios"], 0, 49, 1)
+    result["complementarios"].append(new_number)
 
 
     return result
--- frontend/src/componentes/Bonoloto.js
+++ frontend/src/componentes/Bonoloto.js
@@ -1,58 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import '../estilos/contenedor-boletos.css';
 import '../estilos/Boton.css';
 import '../estilos/cuadro-resultado.css';
 import Boton from "../componentes/Boton";
 
-export default class Bonoloto extends React.Component {
+function Bonoloto() {
 
-    state = { 
-        numeros: [],
-        complementario: []
-    }
+    const [state, setState] = useState({ numeros: [], complementarios: [] });
 
-    generar_bonoloto = () => {
+    const generar_bonoloto = () => {
         axios.get("http://127.0.0.1:5000/bonoloto")
             .then(resultado => {
 
-                const new_state = { 
+                const new_state = {
                     numeros: resultado.data.numeros,
-                    complementario: resultado.data.complementario
+                    complementarios: resultado.data.complementarios
                 };
 
-                this.setState(new_state);
+                setState(new_state);
             })
             .catch(error => console.log(error))
-
-        // fetch("http://127.0.0.1:5000/generar_numeros")
-        //     .then(response => response.json())
-        //     .then(resultado => {
-        //         this.setState({ numeros_generados: resultado })
-        //     })
-        //     .catch(error => console.log(error))
     }
 
-    render() {
-        return (
-            
-            <div className="contenedor-boletos" >  
-                <p>JUEGA</p>
-                
-                <div className="boton">
-                    <Boton Apostar={this.generar_bonoloto}>Generar Apuesta</Boton>
-                </div>
-                <div className="cuadro">
-                    {this.state.numeros_generados.sort((a,b) => a - b).join(", ")}
-                </div>
-                <div className="cuadro">
-                    {this.state.numeros_generados.sort((a,b) => a - b).join(", ")}
-                </div>
-                <h6>0.50 euros por apuesta</h6>
-                
-                
+    return (
+
+        <div className="contenedor-boletos" >
+            <p>JUEGA</p>
+
+            <div className="boton">
+                <Boton Apostar={generar_bonoloto}>Generar Apuesta</Boton>
             </div>
-            
-        )
-    };
+            <div className="cuadro">
+                {state.numeros.sort((a, b) => a - b).join(", ")}
+            </div>
+            <div className="cuadro">
+                {state.complementarios.sort((a, b) => a - b).join(", ")}
+            </div>
+            <h6>0.50 euros por apuesta</h6>
+
+        </div>
+
+    )
 }
+
+export default Bonoloto;
